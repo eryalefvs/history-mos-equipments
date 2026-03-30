f8c8f8bf7a8290dfde03cb33dde0fb720a1cf233
eryalefvs <eryalef@gmail.com>
2026-03-30 17:55:45 -0300

    update header from home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,9 +27,8 @@ function Header() {
     <header className="header">
       <div className="header-inner">
         <Link to="/" className="header-logo">
-          <div className="header-logo-icon">🔧</div>
-          <div className="header-logo-text">
-            Histórico <span>OMs</span>
+          <div className="header-logo-icon">
+            <img src="/src/assets/Vale_logo.png" alt="Logo Vale" />
           </div>
         </Link>
         <nav className="header-nav">
